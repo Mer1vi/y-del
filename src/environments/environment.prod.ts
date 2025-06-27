4c3cc3a67de607c055f4bfa648210209b5b25008
Mer1vi <myagnia37@gmail.com>
2025-06-27 13:12:05 +0100

    Stable version

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCw9OqWGfJ-sbijyfwi9mPwtUqNv2Xrh1w",
+    authDomain: "y-del-ec8b4.firebaseapp.com",
+    projectId: "y-del-ec8b4",
+    storageBucket: "y-del-ec8b4.firebasestorage.app",
+    messagingSenderId: "384825981704",
+    appId: "1:384825981704:web:4e4cbd363b42530d008abd",
+  },
+  googleMapsApiKey: "YOUR API KEY",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCw9OqWGfJ-sbijyfwi9mPwtUqNv2Xrh1w",
+    authDomain: "y-del-ec8b4.firebaseapp.com",
+    projectId: "y-del-ec8b4",
+    storageBucket: "y-del-ec8b4.firebasestorage.app",
+    messagingSenderId: "384825981704",
+    appId: "1:384825981704:web:4e4cbd363b42530d008abd",
+  },
+  googleMapsApiKey: "YOUR API KEY",
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
